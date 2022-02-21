c8734ab61ca5b8269ca5461dac454686d01ac871
judhacdc <jdxxaa@gmail.com>
2022-02-21 11:02:36 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [AboutController::class, 'about']);
 
 
-Route::get('/article/{id}', [ArticleController::class, 'articles']);
+Route::get('/article/{id?}', [ArticleController::class, 'articles']);
